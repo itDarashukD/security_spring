d353794366035b3d2088de0e90773d73ba8dd274
Dima <idDarashukD@g.mail.com>
2021-05-30 15:21:44 +0300

    Create 1st user and his password ,encoding with bcrypt

--- src/main/java/com/example/security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/security/ApplicationSecurityConfig.java
@@ -1,23 +1,50 @@
 package com.example.security.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception { //спрашивает пароль при запросе http,  пароль записан в консоли при загрузке апп
         http.authorizeRequests()
-                .antMatchers("/","index","/css/*","/js/*")// on this page password do not needs/"/8080"
+                .antMatchers("/", "index", "/css/*", "/js/*")// on this page password do not needs/"/8080"
                 .permitAll()//index - file from resouces/static
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {  //We created user
+        UserDetails annaSmithUser = User.builder()
+                .username("annaSmith")
+                .password(passwordEncoder.encode("password")) //aply to "password" BC encoding
+                // now enter http request only with name/password, password automatically encoded with BCryption
+                .roles("STUDENT") // One of the ROLE
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser);
     }
 }
